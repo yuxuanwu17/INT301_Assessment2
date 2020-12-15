ee4f6d66b97daeb85274b0845d0bfc90a4f4e883
Yuxuan <yuxuan.wu17@gmail.com>
2020-12-15 09:36:24 +0800

    rename the file and write into a new file

--- main/ass2.m
+++ main/ass2.m
@@ -6,6 +6,7 @@ clc;
 %% Read the input image 
 imgDataPath = '/Users/yuxuan/Desktop/INT301_Assessment2/ass2data/';
 imgDataDir  = dir(imgDataPath);             % 遍历所有文件
+savepath =  '/Users/yuxuan/Desktop/INT301_Assessment2/ass2_processed_data/'
 for i = 1:length(imgDataDir)
     if(isequal(imgDataDir(i).name,'.')||... % 去除系统自带的两个隐文件夹
        isequal(imgDataDir(i).name,'..')||...
@@ -18,6 +19,9 @@ for i = 1:length(imgDataDir)
     
     for j =1:length(imgDir)                 % 遍历所有图片
         img = imread([imgDataPath imgDataDir(i).name '/' imgDir(j).name]);
+        img_processed = double(img);
+        img_normalized = img_processed/255;
+        imwrite(img_normalized, [savepath,imgDataDir(i).name,num2str(j),'.jpeg'])
     end
 end
 
@@ -26,5 +30,7 @@ files = dir([imgDataPath])
 
 %%
 
-im = imread('/Users/yuxuan/Desktop/INT301_Assessment2/ass2data/B/20130425T121214_character(4545)_4.jpeg')
-size(im)
+% im = imread('/Users/yuxuan/Desktop/INT301_Assessment2/ass2data/B/20130425T121214_character(4545)_4.jpeg')
+% imwrite(im, [savepath,imgDataDir(i).name,num2str(j),'.jpeg'])
+
+
