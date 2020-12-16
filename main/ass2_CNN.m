79e643026be41db68c799f810dd80f10c787637c
Yuxuan <yuxuan.wu17@gmail.com>
2020-12-16 16:03:17 +0800

    ready to use the CNN, but failed to step further

--- main/ass2_CNN.m
+++ main/ass2_CNN.m
@@ -1,2 +1,51 @@
+%%
+clc; 
+clear; 
+
 %% Read the new processed file and output the data and target
-imgDataPath_processed = '/Users/yuxuan/Desktop/INT301_Assessment2/ass2_processed_data/';
+F = '/Users/yuxuan/Desktop/INT301_Assessment2/ass2data'
+imds = imageDatastore(F,'IncludeSubfolders',true,'LabelSource','foldernames')
+
+numTrainFiles = 80;
+[imdsTrain, imdsTest] = splitEachLabel(imds, numTrainFiles,'randomize');
+
+
+%%
+layers = [
+    imageInputLayer([48 24])
+
+    convolution2dLayer(3,8,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    
+    averagePooling2dLayer(2,'Stride',2)
+
+    convolution2dLayer(3,16,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    
+    averagePooling2dLayer(2,'Stride',2)
+  
+    convolution2dLayer(3,32,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    
+    convolution2dLayer(3,32,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    
+    dropoutLayer(0.2)
+    fullyConnectedLayer(1)
+    regressionLayer];
+%%
+
+options = trainingOptions('sgdm', ...
+    'MaxEpochs',30, ...
+    'InitialLearnRate',1e-3, ...
+    'LearnRateSchedule','piecewise', ...
+    'LearnRateDropFactor',0.1, ...
+    'LearnRateDropPeriod',20, ...
+    'Shuffle','every-epoch', ...
+    'ValidationData',{XValidation,YValidation}, ...
+    'Plots','training-progress', ...
+    'Verbose',false);
