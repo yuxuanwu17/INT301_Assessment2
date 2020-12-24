1f2ee066144a2d02964105f62fed20ca5a77ab24
Yuxuan <yuxuan.wu17@gmail.com>
2020-12-24 11:28:20 +0800

    successfully run the rbf with yu's code & need to save the data code

--- main/ass2_rbf.m
+++ main/ass2_rbf.m
@@ -14,6 +14,7 @@ imgDataPath_processed = '/Users/yuxuan/Desktop/INT301_Assessment2/ass2_processed
 X = transpose(X);
 y = transpose(y);
 
+
 %% Seperate the data format in 8 : 2
 [train_idx, test_idx] = crossvalind('HoldOut', y, 0.2);
 X_train = X(train_idx,:);
@@ -26,18 +27,18 @@ stream = RandStream('mlfg6331_64');  % Random number stream
 options = statset('UseParallel',1,'UseSubstreams',1,...
     'Streams',stream);
 
-%% Kmeans analysis 
+%% Kmeans analysis to find the center location
 n_cluster = 160;
 [idx, C] = kmeans(X_train,n_cluster,'Options',options,'MaxIter',10000,...
     'Display','final','Replicates',5);
 
 centers = C';
-%% Calculate the variance of 
+%% Calculate the variance of the rbf kernel 
 
 for i = 1:n_cluster
-dis_square = sum((centers-centers(:,i)).^2); % calculate variance for each center
+dis_square = sqrt(sum((centers-centers(:,i)).^2)); % calculate variance for each center
     curr = max(dis_square); 
-    var_square(i) = curr/(2*n_cluster);
+    var_square(i) = curr;
 end 
 
 %%
@@ -47,7 +48,7 @@ X_train_num = X_train_size(2);
 for i=1:X_train_num  % calculate the k weight matrix
     for j=1:n_cluster 
         dis1 = sqrt(sum((X_train(:,i)-centers(:,j)).^2));
-        K(i,j)=exp(-dis1^2/(2*var_square(j)));  
+        K(i,j)=exp(-dis1^2/(2*var_square(j)^2));  
     end 
 end 
 
@@ -58,13 +59,32 @@ weights=pinv(K'*K)*K'*y_train;
 for i=1:1920 % calculate the simulated training output
     ww1=0; 
     for j=1:n_cluster 
-        dis1 = sqrt(sum((X_train(:,i)-centers(:,j)).^2));
-        curr2=weights(j)*exp(-dis1^2/(2*var_square(j))); 
+        dist = sqrt(sum((X_train(:,i)-centers(:,j)).^2));
+        curr2=weights(j)*exp(-dist^2/(2*var_square(j)^2)); 
         ww1=ww1 + curr2; 
     end 
     y_train_return(i) = ww1; 
 end 
+newtr = int64(y_train_return);
 
+%% Calculate the y test value
+X_test = X_test';
+X_test_size = size(X_test);
+X_test_num = X_test_size(2);
+for i=1:480 % calculate simulated testing output
+    ww2=0; 
+    for j=1:n_cluster 
+        curr11=sqrt(sum((X_test(:,i)-centers(:,j)).^2));
+        curr22=weights(j)*exp(-curr11^2/(2*var_square(j))); 
+        ww2=ww2 + curr22; 
+    end 
+    y_test_return(i) = ww2; 
+end 
+newtt = int64(y_test_return);
+
+%%
+training_acc = rate(newtr,vec2ind(y_train'))
+testing_acc = rate(newtt, vec2ind(y_test'))
 %% Calculate the Gaussian basis function
 % n = 0;
 % X_train_size = size(X_train);
