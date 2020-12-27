58eaac2ad22562d45d2a0a44df8c0f5fbbca9a41
Yuxuan <yuxuan.wu17@gmail.com>
2020-12-27 13:46:09 +0800

    modify the change in som

--- main/ass2_rbf_som.m
+++ main/ass2_rbf_som.m
@@ -2,21 +2,7 @@
 clear;
 close all;
 clc;
-load som_net.mat
-%% Reformat the figure format (uncomment it if you run it first time)
-% figure_preprocessing
-
-% Read the new processed file and output the data and target
-imgDataPath_processed = '/Users/yuxuan/Desktop/INT301_Assessment2/ass2_processed_data/';
-[X, y] = getimdata2(imgDataPath_processed);
-
-%% transpose the X and y for easy manipulation
-X = transpose(X);
-
-%% Seperate the data format in 8 : 2
-[train_idx, test_idx] = crossvalind('HoldOut', y', 0.2);
-X_train = X(train_idx,:);
-X_test = X(test_idx,:);
+load train_test_data.mat 
 %% Define the SOM network
 
 dimensions = [18,20]; % creat a 18X20 neuron matrix %% sqrt(sqrt(1152)*5)=13
@@ -64,7 +50,6 @@ end
 %     end 
 % end 
 %% obtain the weights
-y=transpose(y);
 y_train = y(train_idx,:);
 y_test = y(test_idx,:);
 
